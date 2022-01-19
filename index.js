8bcae8eb016f68873aba331e9180a9fd8aae3684
shiauweitsung <zz830527@gmail.com>
2022-01-19 10:30:34 +0800

    [change] rebase push

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let main = 'main'
+let rebase = 'rebase'
 let mainRebase = 'mainRebase'
 let branch = '分支'
 console.log('main');
 console.log('branch 1');
+console.log('rebase 1');
 console.log('mainRebase 1');
