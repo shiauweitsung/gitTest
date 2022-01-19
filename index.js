cf58968b0e765b956786eb7389961bc9b9a32750
shiauweitsung <zz830527@gmail.com>
2022-01-19 11:01:35 +0800

    [change] main merge push

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let main = 'main'
 let rebase = 'rebase'
 let mainRebase = 'mainRebase'
 let branch = '分支'
+let mainMerge = 'mainMerge'
 console.log('main');
 console.log('branch 1');
 console.log('rebase 1');
 console.log('mainRebase 1');
+console.log('mainMerge 1');
