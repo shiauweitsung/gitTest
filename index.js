1894c88f579e65a1e44cd5753588b1a668307dc6
shiauweitsung <zz830527@gmail.com>
2022-01-19 11:14:25 +0800

    [change] main add 2

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ let mainRebase = 'mainRebase'
 let branch = '分支'
 let mergeBranch = 'mergeBranch'
 let mainMerge = 'mainMerge'
+let mainAdd2 = 'mainAdd2'
 console.log('main');
 console.log('branch 1');
 console.log('rebase 1');
 console.log('mainRebase 1');
 console.log('mergeBranch');
 console.log('mainMerge 1');
-
+console.log('mainAdd2');
